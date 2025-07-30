94ec0565b3822d664493f5409ce2bcecb0e32a47
Devwillmarinho <willmarinho97@gmail.com>
2025-07-30 01:40:31 -0300

    ajustes

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,8 +1,6 @@
-
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
-
 import type { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
@@ -15,6 +13,5 @@ export async function GET(request: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  // Redireciona para o dashboard após o processo de login ser concluído.
   return NextResponse.redirect(requestUrl.origin + '/dashboard')
 }
